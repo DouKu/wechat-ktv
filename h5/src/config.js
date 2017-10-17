24ba415f1daf8221f5f2d9660aff265dbd780832
lgybetter <437675103@qq.com>
2017-10-17 21:51:19 +0800

    build front-end

--- h5/src/config.js
+++ h5/src/config.js
@@ -1,6 +1,6 @@
 export default {
   appId: 'wxed73c69d3bdf1dce',
   redirectUrl: 'http://119.29.193.240',
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'http://119.29.193.240',
   recordId: '13213'
 }
