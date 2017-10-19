fdc9bb1c594483c49ecfbaee54d96f107dca6a6e
QiIL <qsy_skx_code@qq.com>
2017-10-20 00:37:04 +0800

    add cal random point service

--- services/core/user-service.js
+++ services/core/user-service.js
@@ -26,9 +26,32 @@ const updateUser = (filter, data) => {
   })
 }
 
+const makeRandomSongPoint = (audioStr, lyric) => {
+  return new Promise((resolve, reject) => {
+    try {
+      // 正则去除标点符号
+      audioStr = audioStr.replace(/[!！,。，.\?？]/g, "")
+      lyric = lyric.replace(/[!！,，.。\?？]/g, "")
+      let re = new RegExp("(?=.*?)[^" + audioStr +"](?=.*?)|(?=.*?)[^" + lyric + "](?=.*?)", "g");
+      let differentNum = 0;
+      while (re.exec(audioStr + lyric)) differentNum++
+      let proportion = ((audioStr + lyric).length - differentNum) / (audioStr + lyric).length;
+      // 获取低分比较容易
+      if (proportion > 0.4) {
+        return resolve(proportion * 1500);
+      } else {
+        return resolve(proportion * 1000);
+      }
+    } catch (err) {
+      return reject(err)
+    }
+  })
+}
+
 export {
   saveUser,
   findUsers,
 	findOneUser,
-	updateUser
+	updateUser,
+  makeRandomSongPoint
 }
