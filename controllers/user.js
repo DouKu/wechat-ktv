8ecd3c425481a4c7aaa335394444bf296373d500
QiIL <qsy_skx_code@qq.com>
2017-10-22 13:24:24 +0800

    add create user and update user api

--- controllers/user.js
+++ controllers/user.js
@@ -3,9 +3,52 @@
  */
 import { findUsers, saveUser, updateUser } from '../services/core/user-service'
 
-const create = async (req, res, next) => {
+/**
+ * 根据微信返回信息保存用户信息 post
+ * @param {} req 
+ * @param {} res 
+ * @param {} next 
+ */
+const saveUserMessage = async (req, res, next) => {
+  try{
+    await saveUser({
+      openid: req.body.openid,
+      nickname: req.body.nickname,
+      sex: req.body.sex,
+      city: req.body.city,
+      province: req.body.province,
+      country: req.body.country,
+      headimgurl: req.body.headimgurl,
+      privilege: req.body.privilege,
+      access_token: req.body.access_token,
+      refresh_token: req.body.refresh_token,
+      totalScore: req.body.totalScore,
+      realname: req.body.realname,
+      phoneNumber: req.body.phoneNumber
+    })
+    return res.json({ code: 200, msg: "create user success!" })
+  } catch (error) {
+    return res.json({ code: 400, msg: error.message })
+  }
+}
 
+/**
+ * 更新用户信息 put
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+const updateUser = async (req, res, next) => {
+  try{
+    const openid = req.params.openid
+    await updateUser({openid: openid}, {$set: req.body})
+    return res.json({ code: 200, msg: "update user by openid success" })
+  } catch (error) {
+    return res.json({ code: 400, msg: error.message })
+  }
 }
 
 export {
+  saveUserMessage,
+  updateUser
 }
--- routers/user.js
+++ routers/user.js
@@ -1,4 +1,6 @@
-import { } from '../controllers/user'
+import { saveUserMessage, updateUser } from '../controllers/user'
 
 module.exports = (router, commonRouter, authRouter, wechatRouter) => {
+  authRouter.post('/createUser', saveUserMessage)
+  authRouter.put('/updateUser/:openid', updateUser)
 }
