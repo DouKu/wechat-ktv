8d722104540fe1f263aa6695b64f584baa4eba26
QiIL <qsy_skx_code@qq.com>
2017-10-22 12:57:52 +0800

    commit

--- services/core/user-service.js
+++ services/core/user-service.js
@@ -30,14 +30,14 @@ const makeRandomSongPoint = (audioStr, lyric) => {
   return new Promise((resolve, reject) => {
     try {
       // 正则去除标点符号
-      audioStr = audioStr.replace(/[!！,。，.\?？]/g, "")
-      lyric = lyric.replace(/[!！,，.。\?？]/g, "")
+      audioStr = audioStr.replace(/[!！,。，.\?？;；\s+]/g, "")
+      lyric = lyric.replace(/[!！,，.。\?？;；\s+]/g, "")
       let re = new RegExp("(?=.*?)[^" + audioStr +"](?=.*?)|(?=.*?)[^" + lyric + "](?=.*?)", "g");
       let differentNum = 0;
       while (re.exec(audioStr + lyric)) differentNum++
       let proportion = ((audioStr + lyric).length - differentNum) / (audioStr + lyric).length;
       // 获取低分比较容易
-      if (proportion > 0.4) {
+      if (proportion < 0.4) {
         return resolve(proportion * 1500);
       } else {
         return resolve(proportion * 1000);
