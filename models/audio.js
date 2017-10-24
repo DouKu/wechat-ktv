6f2a2ae3e66c84e272827731889bfd6a8e4aff2c
QiIL <qsy_skx_code@qq.com>
2017-10-24 20:54:00 +0800

    add init-audio script

--- models/audio.js
+++ models/audio.js
@@ -7,7 +7,10 @@ const Schema = mongoose.Schema
 const AudioSchema = new Schema({
   url: String,
   name: String,
-  fileName: String
+  fileName: String,
+  secondes: Array,
+  parLen: Array,
+  lyric: Array
 })
 
 export default mongoose.model('Audios', AudioSchema)
