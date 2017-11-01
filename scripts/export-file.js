51d1b1ebfbd25cf54a3dda704a343543fb04b5d2
QiIL <qiusy@healthmall.cn>
2017-11-01 16:13:46 +0800

    fininsh export script

--- scripts/export-file.js
+++ scripts/export-file.js
@@ -3,9 +3,9 @@ import '../mongoose'
 import mongoose from 'mongoose'
 import xlsx from 'node-xlsx'
 import fs from 'fs'
-import { findUsers } from '../services/core/user-service'
+import { findUsers, findOneUser } from '../services/core/user-service'
 import { findChorus } from '../services/core/chorus-service'
-import { findAudio } from '../services/core/audio-service'
+import { findAudio, findOneAudio } from '../services/core/audio-service'
 
 const exportUser = async () => {
   let users = await findUsers({})
@@ -37,23 +37,24 @@ const exportChorus = async () => {
   let chorus = await findChorus({})
   let filename = '合唱信息' + Date.now() + '.xlsx'
   let sheet = [
-    ['序号', '合唱歌曲', '总分',
-    '创建者/微信昵称',
-    '创建者/姓名',
-    '创建者/电话号码',
+    ['序号', '合唱歌曲', '总分', '是否唱完',
+		'创建者/微信昵称', '创建者/姓名', '创建者/电话号码',
     '备注']
-  ]
-  await chorus.forEach( async (item, key) => {
-    let audioMessage = await findAudio({_id: item.audio})
-    let owner = await findUsers({_id: item.owner})
-    let data = [
-      key+1, audioMessage[0].name, item.totalScore,
-      owner[0].nickname, owner[0].name, owner[0].phoneNumber,
-      null
-    ]
-    console.log(data)
-    sheet.push(data);
-  })
+	]
+	let i = 0
+	for (let item of chorus) {
+		i++
+		let owner = await findOneUser({ _id: item.owner })
+		let audio = await findOneAudio({ _id: item.audio })
+		let finish = item.status ? "是" : "否"
+		let buf = [
+			i, audio.name, item.totalScore, finish,
+			owner.nickname, owner.realname, owner.phoneNumber, null,
+		]
+		sheet.push(buf)
+		console.log(sheet)
+	}
+  
   let buff = xlsx.build([{ name: "sheet1", data: sheet }])
   fs.writeFile('scripts/sheet/' + filename, buff, (err) => {
     if (err) throw err
